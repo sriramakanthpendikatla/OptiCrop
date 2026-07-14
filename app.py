d522a88ef44f6133930a7dd210459956fb632b04
Sri Ram <sriramakanthpendikatla84@gmail.com>
2026-07-14 23:36:01 +0530

    Fix Render deployment config

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def predict():
     return render_template('result.html', crop=predicted_crop)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
